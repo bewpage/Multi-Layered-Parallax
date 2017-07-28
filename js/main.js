3ac51a8818cf600d782ce2c399b6b47448ddc14a
bewpage <b.jedlinski@bewpage.com>
2017-07-28 15:12:49 +0200

    update

--- js/main.js
+++ js/main.js
@@ -13,16 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
             var movement = -(topDistance * Number(depth));
             var translate3d = 'translate3d(0, ' + movement + 'px, 0)'
             //console.log(i);
-            console.log(Number(depth));
-            console.log(translate3d);
-            // console.log(parseFloat(depth));
-            //console.log($(element).attr('data-depth'));
-            // layer.style['-webkit-transform'] = translate3d;
-            // layer.style['-moz-transform'] = translate3d;
-            // layer.style['-ms-transform'] = translate3d;
-            // layer.style['-o-transform'] = translate3d;
-            // layer.style.transform = translate3d;
-            //$(layers).css('-webkit-transform', translate3d);
+            // console.log(Number(depth));
+            // console.log(translate3d);
             $(element).css('transform', translate3d);
             $(element).css('-webkit-transform', translate3d);
             $(element).css('-moz-transform', translate3d);
